428a12c7be0a81c9e3a48534896e5b6c71972f2c
rain9441 <rain9441@gmail.com>
2025-11-27 09:05:46 -0600

    feat(): update lsp to be better

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,9 +2,7 @@ local M = {
   {
     'mason-org/mason.nvim',
     cmd = 'Mason',
-    opts = {
-      PATH = 'prepend',
-    },
+    opts = { PATH = 'prepend' },
   },
   {
     'mason-org/mason-lspconfig.nvim',
@@ -29,100 +27,92 @@ local M = {
         filetypes = { 'apexcode', 'apex' },
       })
       vim.lsp.enable('apex_ls')
+      vim.lsp.config('html', {
+        settings = {
+          html = {
+            format = {
+              wrapLineLength = 180,
+              wrapAttributes = 'auto',
+            },
+          },
+        },
+      })
+
+      local format = {
+        indentSwitchCase = true,
+        insertSpaceAfterCommaDelimiter = true,
+        insertSpaceAfterConstructor = false,
+        insertSpaceAfterSemicolonInForStatements = true,
+        insertSpaceBeforeAndAfterBinaryOperators = true,
+        insertSpaceAfterKeywordsInControlFlowStatements = true,
+        insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+        insertSpaceBeforeFunctionParenthesis = false,
+        insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
+        insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
+        insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
+        insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = true,
+        insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
+        insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
+        insertSpaceAfterTypeAssertion = false,
+        placeOpenBraceOnNewLineForFunctions = false,
+        placeOpenBraceOnNewLineForControlBlocks = false,
+        semicolons = 'insert',
+      }
+      local preferences = {
+        quoteStyle = 'single',
+        importModuleSpecifier = 'project-relative',
+        jsxAttributeCompletionStyle = 'auto',
+        allowTextChangesInNewFiles = true,
+        providePrefixAndSuffixTextForRename = true,
+        allowRenameOfImportPath = true,
+        includeAutomaticOptionalChainCompletions = true,
+        provideRefactorNotApplicableReason = true,
+        generateReturnInDocTemplate = true,
+        includeCompletionsForImportStatements = true,
+        includeCompletionsWithSnippetText = true,
+        includeCompletionsWithClassMemberSnippets = true,
+        includeCompletionsWithObjectLiteralMethodSnippets = true,
+        useLabelDetailsInCompletionEntries = true,
+        allowIncompleteCompletions = true,
+        displayPartsForJSDoc = true,
+        disableLineTextInReferences = true,
+        includeInlayParameterNameHints = 'none',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = false,
+        includeInlayVariableTypeHints = false,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = false,
+        includeInlayFunctionLikeReturnTypeHints = false,
+        includeInlayEnumMemberValueHints = false,
+      }
+
+      vim.lsp.config(
+        'vtsls',
+        vim.tbl_extend('force', require('vtsls').lspconfig, {
+          settings = {
+            complete_function_calls = true,
+            typescript = {
+              updateImportsOnFileMove = 'always',
+              preferences = preferences,
+              format = format,
+              tsserver = {
+                experimental = {
+                  -- enableProjectDiagnostics = true
+                },
+              },
+            },
+            javascript = {
+              updateImportsOnFileMove = 'always',
+              preferences = preferences,
+              format = format,
+            },
+          },
+        })
+      )
 
       require('mason-lspconfig').setup({
         ensure_installed = {},
-        automatic_enable = {
-          exclude = { 'stylua' },
-        },
-        handlers = {
-          function(server) require('lspconfig')[server].setup({}) end,
-          ['angularls'] = function()
-            require('lspconfig').angularls.setup({
-              root_dir = require('lspconfig.util').root_pattern('angular.json', 'project.json', 'nx.json', '.git'),
-            })
-          end,
-          ['apex_ls'] = function()
-            require('lspconfig').apex_ls.setup({
-              filetypes = { 'apexcode', 'apex' },
-              apex_enable_semantic_errors = false, -- Whether to allow Apex Language Server to surface semantic errors
-              apex_enable_completion_statistics = false, -- Whether to allow Apex Language Server to collect telemetry on code completion usage
-            })
-          end,
-          ['vtsls'] = function()
-            local format = {
-              indentSwitchCase = true,
-              insertSpaceAfterCommaDelimiter = true,
-              insertSpaceAfterConstructor = false,
-              insertSpaceAfterSemicolonInForStatements = true,
-              insertSpaceBeforeAndAfterBinaryOperators = true,
-              insertSpaceAfterKeywordsInControlFlowStatements = true,
-              insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
-              insertSpaceBeforeFunctionParenthesis = false,
-              insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
-              insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
-              insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
-              insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = true,
-              insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
-              insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
-              insertSpaceAfterTypeAssertion = false,
-              placeOpenBraceOnNewLineForFunctions = false,
-              placeOpenBraceOnNewLineForControlBlocks = false,
-              semicolons = 'insert',
-            }
-            local preferences = {
-              quoteStyle = 'single',
-              importModuleSpecifier = 'project-relative',
-              jsxAttributeCompletionStyle = 'auto',
-              allowTextChangesInNewFiles = true,
-              providePrefixAndSuffixTextForRename = true,
-              allowRenameOfImportPath = true,
-              includeAutomaticOptionalChainCompletions = true,
-              provideRefactorNotApplicableReason = true,
-              generateReturnInDocTemplate = true,
-              includeCompletionsForImportStatements = true,
-              includeCompletionsWithSnippetText = true,
-              includeCompletionsWithClassMemberSnippets = true,
-              includeCompletionsWithObjectLiteralMethodSnippets = true,
-              useLabelDetailsInCompletionEntries = true,
-              allowIncompleteCompletions = true,
-              displayPartsForJSDoc = true,
-              disableLineTextInReferences = true,
-              includeInlayParameterNameHints = 'none',
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = false,
-              includeInlayVariableTypeHints = false,
-              includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-              includeInlayPropertyDeclarationTypeHints = false,
-              includeInlayFunctionLikeReturnTypeHints = false,
-              includeInlayEnumMemberValueHints = false,
-            }
-
-            require('lspconfig').vtsls.setup({
-              root_dir = require('lspconfig.util').root_pattern('angular.json', 'project.json', 'nx.json', '.git'),
-              settings = {
-                completions = { completeFunctionCalls = true },
-                javascript = { format = format },
-                typescript = { format = format },
-              },
-              init_options = {
-                preferences = preferences,
-              },
-            })
-          end,
-          ['html'] = function()
-            require('lspconfig').html.setup({
-              settings = {
-                html = {
-                  format = {
-                    wrapLineLength = 180,
-                    wrapAttributes = 'auto',
-                  },
-                },
-              },
-            })
-          end,
-        },
+        automatic_enable = { exclude = { 'stylua' } },
       })
     end,
   },
@@ -180,7 +170,6 @@ local M = {
     'yioneko/nvim-vtsls',
     ft = { 'javascript', 'typescript', 'js', 'ts' },
     dependencies = { 'neovim/nvim-lspconfig', 'mason-org/mason-lspconfig.nvim' },
-    config = function() require('lspconfig.configs').vtsls = require('vtsls').lspconfig end,
   },
   {
     'Hoffs/omnisharp-extended-lsp.nvim',
